91cdf7b424ee6484c5ac7f51114191a9f97b8995
Hansol Lee <lhs@partdb.com>
2021-08-18 17:59:47 +0900

    project 생성

---
+++ src/main/java/com/hansoleee/hsspringsecurity/HsSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hansoleee.hsspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hansoleee/hsspringsecurity/HsSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansoleee.hsspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
