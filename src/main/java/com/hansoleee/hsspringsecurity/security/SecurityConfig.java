0a89ce63672ddbad36ba989eb9b96fbc5d9576c0
lhs <leehansol.kr@gmail.com>
2021-08-18 20:37:46 +0900

    로그인, 회원가입 페이지 permitAll 설정

---
+++ src/main/java/com/hansoleee/hsspringsecurity/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.hansoleee.hsspringsecurity.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Slf4j
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login", "/register").permitAll();
+    }
+}
